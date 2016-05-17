d3ecd78e1c1539a7b248e368863330ace97b742c
sunqifs <sunqi.thu@gmail.com>
2016-05-17 17:20:56 +0800

    Added Idea project HelloWorld. This repo will be used for Java learning.

---
+++ HelloWorld/src/quickstart/HelloWorld.java
@@ -0,0 +1,10 @@
+package quickstart;
+
+/**
+ * Created by sunqi on 5/17/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
